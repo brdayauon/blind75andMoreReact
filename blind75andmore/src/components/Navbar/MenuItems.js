b2ec29f69d2b27398c071adc08bf90f1987cb841
Brandon Dayauon <brdayauon@gmail.com>
2021-08-15 20:36:03 -0700

    made navbar

---
+++ blind75andmore/src/components/Navbar/MenuItems.js
@@ -0,0 +1,59 @@
+export const MenuItems = [
+    
+    {
+        title: 'Array',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Binary',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'DP',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Graph',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Interval',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Linked List',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Matrix',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'String',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Tree',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Heap',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+
+]
